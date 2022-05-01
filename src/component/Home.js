71408e0b5f0a7eeb7b6756f8c2c70488fc1e51df
Julian Aryo <julian.aryo1989@gmail.com>
2022-05-02 06:20:02 +0800

    bpp

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,18 +1,77 @@
 import styled from "@emotion/styled";
-import { Button, Card, Container, Grid } from "@mui/material";
+import {
+  Avatar,
+  Button,
+  Card,
+  Chip,
+  Container,
+  Divider,
+  Grid,
+  ListItem,
+  Paper,
+  Typography,
+} from "@mui/material";
 
 export default function Home() {
+  const coachStyle = {
+    backgroundColor: "#efefef",
+    minHeight: "300px",
+    textAlign: "center",
+  };
+
+  const playerStyle = {
+    backgroundColor: "#efefef",
+    minHeight: "60px",
+    textAlign: "center",
+  };
+
   return (
     <div className="App">
       <div>
-        <Grid container rowSpacing={1}>
-          <Grid item xs={12}>
-            <Card>1</Card>
+        <Container maxWidth="lg">
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Grid container spacing={2}>
+                <Grid item md={6} xs={12}>
+                  <Paper elevation={0}>
+                    <Typography variant="h2">90</Typography>
+                  </Paper>
+                </Grid>
+                <Grid item md={6} xs={12} style={{ textAlign: "right" }}>
+                  <Typography variant="h2">Player 1 turn </Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+            {[...Array(3).keys()].map((item, index) => {
+              return (
+                <Grid item md={4} xs={12} key={index}>
+                  <Paper elevation={0} style={coachStyle}>
+                    <h1>Player {index}</h1>
+                    {[...Array(7).keys()].map((item, index) => {
+                      return (
+                        <Chip
+                          key={index}
+                          avatar={<Avatar>QB</Avatar>}
+                          label={"Player " + index}
+                          style={{ margin: "4px" }}
+                        />
+                      );
+                    })}
+                  </Paper>
+                </Grid>
+              );
+            })}
+            {[...Array(120).keys()].map((item, index) => {
+              return (
+                <Grid item md={1} xs={6} key={index}>
+                  <Paper elevation={0} style={playerStyle}>
+                    {index}
+                  </Paper>
+                </Grid>
+              );
+            })}
           </Grid>
-          <Grid item xs={12}>
-            <Card>1</Card>
-          </Grid>
-        </Grid>
+        </Container>
       </div>
     </div>
   );
